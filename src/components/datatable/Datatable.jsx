ebee60ea0b8f15006eefafaccff9bac66688945f
Raju1-Ahmed <robiussaniraju@gmail.com>
2023-07-18 16:30:39 +0600

    user delete add

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -1,14 +1,50 @@
 import "./datatable.scss";
 import { DataGrid } from "@mui/x-data-grid";
-import { userColumns, userRows } from "../../datatablesource";
-import { Link } from "react-router-dom";
-import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
+import useFetch from "../../hooks/useFetch";
+import axios from "axios";
+import Loading from "../loading/Loading";
+import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from "@mui/material";
+import { userColumns } from "../../datatablesource";
 
 const Datatable = () => {
-  const [data, setData] = useState(userRows);
+  const location = useLocation();
+  const path = location.pathname.split("/")[1];
+  const [list, setList] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [showConfirmation, setShowConfirmation] = useState(false);
+  const [deleteItemId, setDeleteItemId] = useState(null);
+  const { data, loading: fetchDataLoading, error } = useFetch(`http://localhost:8800/api/${path}`);
 
-  const handleDelete = (id) => {
-    setData(data.filter((item) => item.id !== id));
+  useEffect(() => {
+    if (data && data.length > 0) {
+      setList(data);
+    }
+  }, [data]);
+
+  const handleDelete = async () => {
+    setLoading(true);
+    try {
+      await axios.delete(`http://localhost:8800/api/${path}/${deleteItemId}`);
+      setList(list.filter((item) => item._id !== deleteItemId));
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setLoading(false);
+      setDeleteItemId(null);
+      setShowConfirmation(false);
+    }
+  };
+
+  const handleDeleteConfirmation = (id) => {
+    setDeleteItemId(id);
+    setShowConfirmation(true);
+  };
+
+  const handleCancelConfirmation = () => {
+    setDeleteItemId(null);
+    setShowConfirmation(false);
   };
 
   const actionColumn = [
@@ -17,15 +53,13 @@ const Datatable = () => {
       headerName: "Action",
       width: 200,
       renderCell: (params) => {
+        const itemId = params.row._id;
         return (
           <div className="cellAction">
-            <Link to="/users/test" style={{ textDecoration: "none" }}>
+            <Link to={`/users/${itemId}`} style={{ textDecoration: "none" }}>
               <div className="viewButton">View</div>
             </Link>
-            <div
-              className="deleteButton"
-              onClick={() => handleDelete(params.row.id)}
-            >
+            <div className="deleteButton" onClick={() => handleDeleteConfirmation(itemId)}>
               Delete
             </div>
           </div>
@@ -33,6 +67,15 @@ const Datatable = () => {
       },
     },
   ];
+
+  if (fetchDataLoading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
     <div className="datatable">
       <div className="datatableTitle">
@@ -43,12 +86,26 @@ const Datatable = () => {
       </div>
       <DataGrid
         className="datagrid"
-        rows={data}
+        rows={list}
         columns={userColumns.concat(actionColumn)}
         pageSize={9}
         rowsPerPageOptions={[9]}
         checkboxSelection
+        getRowId={(row) => row._id}
       />
+
+      <Dialog open={showConfirmation} onClose={handleCancelConfirmation}>
+        <DialogTitle>Confirm Delete</DialogTitle>
+        <DialogContent>
+          Are you sure you want to delete this item?
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCancelConfirmation}>Cancel</Button>
+          <Button onClick={handleDelete}>Delete</Button>
+        </DialogActions>
+      </Dialog>
+
+      {loading && <Loading />}
     </div>
   );
 };
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -3,10 +3,69 @@ import Sidebar from "../../components/sidebar/Sidebar";
 import Navbar from "../../components/navbar/Navbar";
 import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
 import { useState } from "react";
+import axios from "axios";
 
 const New = ({ inputs, title }) => {
   const [file, setFile] = useState("");
+  const [info, setInfo] = useState({});
+  const [errorMessage, setErrorMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
 
+  const handleChange = (e) => {
+    setInfo((prev) => ({ ...prev, [e.target.id]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+
+    if (!info.name) {
+      setErrorMessage("Please enter a name.");
+      return;
+    }
+
+    const data = new FormData();
+    data.append("file", file);
+    data.append("upload_preset", "upload");
+    try {
+      const uploadRes = await axios.post(
+        "https://api.cloudinary.com/v1_1/dmrxamgbh/image/upload",
+        data
+      );
+
+      const { url } = uploadRes.data;
+
+      const newUser = {
+        ...info,
+        img: url,
+      };
+
+      await axios.post("http://localhost:8800/api/auth/register", newUser);
+      setSuccessMessage("Data and file are successfully inserted.");
+
+      // Clear the form and file input after successful insertion
+      setInfo({});
+      setFile(null);
+    } catch (err) {
+      if (err.response) {
+        // The request was made and the server responded with a status code
+        const errorMessage = err.response.data.message;
+        setErrorMessage(errorMessage);
+        console.log(errorMessage);
+      } else if (err.request) {
+        // The request was made but no response was received
+        const errorMessage = "Request error. Please try again.";
+        setErrorMessage(errorMessage);
+        console.log(err.request);
+      } else {
+        // Something happened in setting up the request
+        const errorMessage = "An unexpected error occurred.";
+        setErrorMessage(errorMessage);
+        console.log(err.message);
+      }
+    }
+  };
+
+  console.log(info);
   return (
     <div className="new">
       <Sidebar />
@@ -43,13 +102,24 @@ const New = ({ inputs, title }) => {
               {inputs.map((input) => (
                 <div className="formInput" key={input.id}>
                   <label>{input.label}</label>
-                  <input type={input.type} placeholder={input.placeholder} />
+                  <input
+                    onChange={handleChange}
+                    type={input.type}
+                    placeholder={input.placeholder}
+                    id={input.id}
+                  />
                 </div>
               ))}
-              <button>Send</button>
+              <button onClick={handleClick}>Send</button>
             </form>
           </div>
         </div>
+        {errorMessage && <p className="error">{errorMessage}</p>}
+        {successMessage && (
+          <div className="popup">
+            <p>{successMessage}</p>
+          </div>
+        )}
       </div>
     </div>
   );
